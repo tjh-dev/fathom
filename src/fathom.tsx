7dc26024a9450076e7855f74219d3e1344512938
Thom <14987462+thomhayward@users.noreply.github.com>
2023-07-07 18:37:15 +0100

    add options

--- src/fathom.tsx
+++ src/fathom.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { fathomScript } from "./";
 
-const fathomUrl = "https://cdn.usefathom.com/script.js";
+export type FathomScriptProps = {
+	/**
+	 * Site ID
+	 */
+	site: string;
+	/**
+	 * Array of domains to exclude from tracking.
+	 */
+	exclude?: string[];
+	/**
+	 * Use canonical url if one is defined.
+	 *
+	 * Set to "false" to override the default behaviour.
+	 */
+	canonical?: boolean;
+	/**
+	 * Disable automatic tracking.
+	 *
+	 * Set to "false" to override the default behaviour.
+	 */
+	auto?: boolean;
+};
 
-function FathomScript({ site }: { site: string }) {
-	return <script src={fathomUrl} data-site={site} data-spa="auto" defer />;
+function FathomScript({ site, exclude, canonical, auto }: FathomScriptProps) {
+	return (
+		<script
+			src={fathomScript}
+			data-site={site}
+			data-excluded-domains={exclude?.map((domain) => domain.trim()).join(",")}
+			data-canonical={canonical}
+			data-auto={auto}
+			data-spa="history"
+			defer
+		/>
+	);
 }
 
 export const Fathom = React.memo(FathomScript);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export const fathomDomain = "https://cdn.usefathom.com";
+export const fathomScript = fathomDomain + "/script.js";
+
 export { Fathom } from "./fathom";
 export { useGoal } from "./use-goal";
