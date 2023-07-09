a12518c2b0ff4f8aa676a59e589e0ae5a8035a14
Thom <14987462+thomhayward@users.noreply.github.com>
2023-07-09 06:31:39 +0100

    allow overriding script url

--- build/fathom.js
+++ build/fathom.js
@@ -7,7 +7,7 @@ exports.Fathom = void 0;
 var react_1 = __importDefault(require("react"));
 var _1 = require("./");
 function FathomScript(_a) {
-    var site = _a.site, exclude = _a.exclude, canonical = _a.canonical, auto = _a.auto;
-    return (react_1.default.createElement("script", { src: _1.fathomScript, "data-site": site, "data-excluded-domains": exclude === null || exclude === void 0 ? void 0 : exclude.map(function (domain) { return domain.trim(); }).join(","), "data-canonical": canonical, "data-auto": auto, "data-spa": "history", defer: true }));
+    var site = _a.site, exclude = _a.exclude, canonical = _a.canonical, auto = _a.auto, scriptUrl = _a.scriptUrl;
+    return (react_1.default.createElement("script", { src: scriptUrl !== null && scriptUrl !== void 0 ? scriptUrl : _1.fathomScript, "data-site": site, "data-excluded-domains": exclude === null || exclude === void 0 ? void 0 : exclude.map(function (domain) { return domain.trim(); }).join(","), "data-canonical": canonical, "data-auto": auto, "data-spa": "history", defer: true }));
 }
 exports.Fathom = react_1.default.memo(FathomScript);
